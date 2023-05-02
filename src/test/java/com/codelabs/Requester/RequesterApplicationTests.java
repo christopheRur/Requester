ff2608913a920171c8217faa546ccee7104a45fa
Rurangwa <chrisruu@yahoo.com>
2023-05-02 01:16:14 -0400

    adding test testsendmessage

--- src/test/java/com/codelabs/Requester/RequesterApplicationTests.java
+++ src/test/java/com/codelabs/Requester/RequesterApplicationTests.java
@@ -1,13 +1,49 @@
 package com.codelabs.Requester;
 
+import com.codelabs.Requester.Controller.RequesterController;
+import com.codelabs.Requester.model.Sender;
+import com.codelabs.Requester.service.RequesterService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 class RequesterApplicationTests {
 
+	@Mock
+	private Sender sender;
+
+	@Mock
+	private RequesterService reqServ;
+
+	@InjectMocks
+	RequesterController controller;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testSendMessage(){
+		sender.setMessage("here is the message");
+
+		Mockito.when(reqServ.sendMessage(sender)).thenReturn(sender);
+
+		ResponseEntity<?> response= controller.sendMessage(reqServ.sendMessage(sender));
+
+		assertEquals(HttpStatus.OK,response.getStatusCode());
+
 	}
 
 }
